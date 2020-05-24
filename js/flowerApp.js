62377d5f2a656db39c9ff70151fc96e364dd8db3
dut-a <d.dut@live.com>
2020-05-23 22:09:32 -0500

    Adds Model properties

---
+++ js/flowerApp.js
@@ -0,0 +1,31 @@
+
+var redRoses = new app.singleFlower({
+  name: "Red Roses",
+  price: 39.95,
+  color: "Red",
+  img: "images/red-roses.jpg",
+  link: "redRose"
+});
+
+var rainbowRoses = new app.singleFlower({
+  name: "Rainbow Roses",
+  price: 29.95,
+  color: "orange",
+  link: "rainbowRose"
+});
+
+var heirloomRoses = new app.singleFlower({
+  name: "Heirloom roses",
+  price: 19.95,
+  img: "images/heirloom-pink-roses.jpg",
+  link: "heirloomRose"
+});
+
+var flowers = [
+  heirloomRoses,
+  rainbowRoses,
+  redRoses
+];
+
+flowers.forEach(f => console.log(f.toJSON()));
+
