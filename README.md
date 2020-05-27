4286df3050b9c90a9a82f24092e4eb0b98ffdf73
dut-a <d.dut@live.com>
2020-05-27 17:19:30 -0500

    Adds more flowers

--- README.md
+++ README.md
@@ -14,8 +14,11 @@
 
   3. [Backbone.js](https://backbonejs.org) (the main deal)
 
+## Other
+  Most images from [https://unsplash.com](https://unsplash.com/)
 
-## Other resources
+
+## Resources
   1. _{Book}_ Developing Backbone.js Applications: [Open source/free](https://addyosmani.com/backbone-fundamentals), [On Amazon.com](https://www.amazon.com/_/dp/1449328253?tag=oreilly20-20)
 
   2. _{Book}_ Full Stack Web Development with Backbone.js: [On Amazon.com](https://www.amazon.com/Full-Stack-Web-Development-Backbone-js-dp-1449370985/dp/1449370985/ref=mt_paperback?_encoding=UTF8&me=&qid=)
--- js/flowerApp.js
+++ js/flowerApp.js
@@ -21,10 +21,77 @@ var heirloomRoses = new app.singleFlower({
   link: "heirloomRose"
 });
 
+var others = [
+  new app.singleFlower({
+    name: "Other flower 1",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-1.jpeg",
+    link: "other-flower-1"
+  }),
+  new app.singleFlower({
+    name: "Other flower 2",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-2.jpeg",
+    link: "other-flower-2"
+  }),
+  new app.singleFlower({
+    name: "Other flower 3",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-3.jpeg",
+    link: "other-flower-3"
+  }),
+  new app.singleFlower({
+    name: "Other flower 4",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-4.jpeg",
+    link: "other-flower-4"
+  }),
+  new app.singleFlower({
+    name: "Other flower 5",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-5.jpeg",
+    link: "other-flower-5"
+  }),
+  new app.singleFlower({
+    name: "Other flower 6",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-6.jpeg",
+    link: "other-flower-6"
+  }),
+  new app.singleFlower({
+    name: "Other flower 7",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-7.jpeg",
+    link: "other-flower-7"
+  }),
+  new app.singleFlower({
+    name: "Other flower 8",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-8.jpeg",
+    link: "other-flower-8"
+  }),
+  new app.singleFlower({
+    name: "Other flower 9",
+    price: 28.95,
+    color: "Other",
+    img: "images/other-flower-9.jpeg",
+    link: "other-flower-9"
+  }),
+];
+
 var flowers = [
   heirloomRoses,
   rainbowRoses,
-  redRoses
+  redRoses,
+  ...others
 ];
 
 flowers.forEach(f => console.log(f.toJSON()));
